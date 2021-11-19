39de4ee5a7b97e40a594bcb720f782e6d744f66f
Kemal Efendioğlu <kemal@Kemal-MacBook-Pro.local>
2021-11-20 00:28:16 +0300

    feat: add satellite entity models

---
+++ app/src/main/java/com/efendioglu/satellitetracker/data/model/Satellite.kt
@@ -0,0 +1,33 @@
+package com.efendioglu.satellitetracker.data.model
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class Satellite(
+    @SerialName("id") val id: Int,
+    @SerialName("active") val active: Boolean,
+    @SerialName("name") val name: String
+)
+
+
+@Serializable
+data class SatelliteDetail(
+    @SerialName("id") val id: Int,
+    @SerialName("cost_per_launch") val costPerLaunch: Int,
+    @SerialName("first_flight") val firstFlight: String,
+    @SerialName("height") val height: Int,
+    @SerialName("mass") val mass: Int,
+)
+
+@Serializable
+data class SatellitePositions(
+    @SerialName("id") val id: Int,
+    @SerialName("positions") val positions: List<Position> = emptyList()
+)
+
+@Serializable
+data class Position(
+    @SerialName("posX") val posX: Double,
+    @SerialName("posY") val posY: Double,
+)
